c08ea9eea6bbc46526034ac8c747b2168efff14e
sowabdulai <abdulai-as@hotmail.com>
2022-02-01 09:13:10 -0500

    user score board

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //the variable
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
@@ -18,7 +18,10 @@ function getComputerChoice() {
 }           
 
 function win() {
-  console.log('win');
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  
 }
 
 function lose() {
